e87954037cd1159cac8b1925fd3906d40c285ec4
KangWon LEE <kangwon.lee@kpu.ac.kr>
2018-11-22 09:55:46 +0900

    Added get_sections_dict()
    Using the config info, build dict containing list of repositories of all sections

--- collect_chapters.py
+++ collect_chapters.py
@@ -1,15 +1,33 @@
 """
 Collect repositories of the chapters into a submodule
 """
+
+import ast
 import configparser
+import pprint
 import sys
 import urllib.parse as up
+
+import regex_test as ret
 
 
 def main(argv):
     config = configparser.ConfigParser()
     config.read(argv[0])
-    print(config['operation']['sections'])
+
+    pprint.pprint(get_sections_dict(config))
+
+
+def get_sections_dict(config):
+    """
+    Using the config info, build dict containing list of repositories of all sections
+    """
+    sections_dict = {}
+
+    for section in ast.literal_eval(config['operation']['sections']):
+        sections_dict[section] = ret.get_github_url_list(config[section]['list'].strip())
+
+    return sections_dict
 
 
 if "__main__" == __name__:
