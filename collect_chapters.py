d588b94eb3b5439af4fdd54fa90094168ba7ef62
KangWon LEE <kangwon.lee@kpu.ac.kr>
2018-11-22 10:19:42 +0900

    Added transpose_dict() :
        """
        Input
        =====
        section0: [repo0_00, repo0_01, ...],
        section1: [repo1_00, repo1_01, ...],
    
        Output
        =====
        id00: {section0: repo0_00, section1: repo1_00, ...},
        id01: {section0: repo0_01, section1: repo1_01, ...},
        """

--- collect_chapters.py
+++ collect_chapters.py
@@ -4,6 +4,7 @@ Collect repositories of the chapters into a submodule
 
 import ast
 import configparser
+import os
 import pprint
 import sys
 import urllib.parse as up
@@ -15,7 +16,7 @@ def main(argv):
     config = configparser.ConfigParser()
     config.read(argv[0])
 
-    pprint.pprint(get_sections_dict(config))
+    pprint.pprint(transpose_dict(get_sections_dict(config)))
 
 
 def get_sections_dict(config):
@@ -28,6 +29,34 @@ def get_sections_dict(config):
         sections_dict[section] = ret.get_github_url_list(config[section]['list'].strip())
 
     return sections_dict
+
+
+def transpose_dict(sections_dict):
+    """
+    Input
+    =====
+    section0: [repo0_00, repo0_01, ...],
+    section1: [repo1_00, repo1_01, ...],
+
+    Output
+    =====
+    id00: {section0: repo0_00, section1: repo1_00, ...},
+    id01: {section0: repo0_01, section1: repo1_01, ...},
+    """
+
+    ids_dict = {}
+
+    for section in sections_dict:
+        for url in sections_dict[section]:
+            parse = up.urlparse(url)
+            # user_id is the last part of the url path except extension if there is
+            user_id = os.path.splitext(os.path.split(parse.path)[-1])[0].split('-')[-1]
+            # add url to the table
+            user_dict = ids_dict.get(user_id, {})
+            user_dict[section] = url
+            ids_dict[user_id] = user_dict
+
+    return ids_dict
 
 
 if "__main__" == __name__:
