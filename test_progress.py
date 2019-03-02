cab0579991748a2102fb4162d94a711313f76b78
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-03-02 14:52:55 +0900

    test_get_commit_dict_03() : applied get_git_log_commit_line()

--- test_progress.py
+++ test_progress.py
@@ -599,12 +599,29 @@ class TestRepoEvalCountOneCommitLog(unittest.TestCase):
     def test_get_commit_dict_03(self):
 
         txt = '{\'sha\':\'94e4ecc8bd8d02121b0824b14c5e7fed4459e2f8\', \'author\':u\'\'\'name\'\'\', \'email\':u\'email@email.com\', \'date\':\'Fri Mar 30 12:02:40 2018 +0900\', \'subject\': u\'\'\'"""가 print 명령어를 반복하지 않아도 되게 한다. 백슬래쉬를 사용하는 방법. (줄을 변경하게 함. 엔터효과)\\\\\\\'\'\'}'
+
+        sha = '94e4ecc8bd8d02121b0824b14c5e7fed4459e2f8'
+        author = u'name'
+        email = u'email@email.com'
+        date = 'Fri Mar 30 12:02:40 2018 +0900'
+        subject = u'''"""가 print 명령어를 반복하지 않아도 되게 한다. 백슬래쉬를 사용하는 방법. (줄을 변경하게 함. 엔터효과)\\\\\\'''
+
+        txt = self.get_git_log_commit_line(
+            sha, 
+            author, 
+            email, date, subject,
+        )
+
         result = self.e.get_commit_dict(txt)
-        expected = {'sha': '94e4ecc8bd8d02121b0824b14c5e7fed4459e2f8', 
-                    'author': 'Name', 
-                    'email': 'email@domain.name', 
-                    'date': 'Fri Mar 30 12:02:40 2018 +0900', 
-                    'subject': '''"""가 print 명령어를 반복하지 않아도 되게 한다. 백슬래쉬를 사용하는 방법. (줄을 변경하게 함. 엔터효과)\\\\\\'''}
+
+        expected = {
+            'sha': sha, 
+            'author': author, 
+            'email': email, 
+            'date': date, 
+            'subject': subject, 
+        }
+
         # """가 print 명령어를 반복하지 않아도 되게 한다.
         # 백슬래쉬를 사용하는 방법. (줄을 변경하게 함. 엔터효과)\\\
         self.maxDiff=None
