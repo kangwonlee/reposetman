d947e459e7a05a1d64f283ad95b8b0bc57e974bf
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-04-11 19:09:04 +0900

    added onerror() :
    # https://stackoverflow.com/questions/2656322/shutil-rmtree-fails-on-windows-with-access-is-denied
    # https://stackoverflow.com/questions/1213706/what-user-do-python-scripts-run-as-in-windows

--- test_ret.py
+++ test_ret.py
@@ -5,6 +5,30 @@ import unittest
 import regex_test as ret
 
 
+def onerror(func, path, exc_info):
+    """
+    Error handler for ``shutil.rmtree``.
+
+    If the error is due to an access error (read only file)
+    it attempts to add write permission and then retries.
+
+    If the error is for another reason it re-raises the error.
+
+    Usage : ``shutil.rmtree(path, onerror=onerror)``
+
+    # https://stackoverflow.com/questions/2656322/shutil-rmtree-fails-on-windows-with-access-is-denied
+    # https://stackoverflow.com/questions/1213706/what-user-do-python-scripts-run-as-in-windows
+
+    """
+    import stat
+    if not os.access(path, os.W_OK):
+        # Is the error an access error ?
+        os.chmod(path, stat.S_IWUSR)
+        func(path)
+    else:
+        raise IOError
+
+
 class TestFetchAndReset(unittest.TestCase):
     def setUp(self):
 
