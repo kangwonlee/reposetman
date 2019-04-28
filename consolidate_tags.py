0292dd35b1cbfe7b2b3f947874c138528cf31fb7
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-04-28 22:37:56 +0900

    process_tags() : raise exception for master branch only

--- consolidate_tags.py
+++ consolidate_tags.py
@@ -70,15 +70,20 @@ def process_tags():
         if 2 == len(tag_split_list):
             date_time_str, small_sha = tag_split_list
         elif 3 == len(tag_split_list):
-            date_time_str, _, small_sha = tag_split_list
+            date_time_str, branch_name, small_sha = tag_split_list
         elif 4 == len(tag_split_list):
-            date_time_str0, date_time_str1, _, small_sha = tag_split_list
+            date_time_str0, date_time_str1, branch_name, small_sha = tag_split_list
             date_time_str = '_'.join([date_time_str0, date_time_str1])
         else:
             print(tag_split_list)
             raise(ValueError)
 
-        assert sha.startswith(small_sha), f"sha = {sha}, small_sha = {small_sha}"
+        if not sha.startswith(small_sha):
+
+            print(f"sha = {sha}, small_sha = {small_sha}")
+
+            if 'master' == branch_name:
+                raise ValueError(f"sha = {sha}, small_sha = {small_sha}")
 
         # Tue_Apr_24_16_54_15_2018
         datetime_struct = time.strptime(date_time_str, '%a_%b_%d_%H_%M_%S_%Y')
