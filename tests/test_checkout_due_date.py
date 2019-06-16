ef480e24b6cd697d3916c57387fc4bdc813d4b69
KangWon LEE u64 <kangwon.lee.edu@gmail.com>
2019-06-17 08:29:58 +0900

    test get_arg_parser() for config file name

--- tests/test_checkout_due_date.py
+++ tests/test_checkout_due_date.py
@@ -12,3 +12,18 @@ sys.path.insert(0,
 )
 
 import checkout_due_date as cdd
+
+
+class TestGetArgParser(unittest.TestCase):
+    def test_get_arg_parser_config_filename(self):
+        p = cdd.get_arg_parser()
+
+        namespace = p.parse_args(['--config', 'config_filename.cfg'])
+
+        self.assertIn('config', namespace)
+        self.assertEqual('config_filename.cfg', namespace.config)
+        self.assertFalse(namespace.date)
+
+
+if "__main__" == __name__:
+    unittest.main()
