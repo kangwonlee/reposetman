1c21e9a0ee9c5b7f0bc106680703b13d350c1abf
KangWon LEE u64 <kangwon.lee.edu@gmail.com>
2019-06-18 16:36:22 +0900

    test gen_repo_path()

--- tests/test_checkout_due_date.py
+++ tests/test_checkout_due_date.py
@@ -1,5 +1,7 @@
+import configparser
 import os
 import sys
+import tempfile
 import unittest
 
 sys.path.insert(0,
@@ -42,6 +44,64 @@ class TestGetArgParser(unittest.TestCase):
         self.assertEqual(namespace.date, '2019-06-08')
         self.assertIn('time', namespace)
         self.assertEqual(namespace.time, '01:23:45')
+
+
+class TestGenRepoPath(unittest.TestCase):
+    def setUp(self):
+        self.list_file = tempfile.NamedTemporaryFile(
+            suffix='.txt',
+            mode='wt',
+            encoding='utf-8',
+            )
+
+        self.list_file.write(
+            "가나다(2082652342) (03.11 오후 02:33)\n"
+            "\n"
+            "2082652342 가나다  https://github.com/CPF18A/18pfa_lpthw-abc.git\n"
+            "\n"
+            "라마바(2018007194) (03.11 오후 02:33)\n"
+            "\n"
+            "2018007194 라마바 https://def@github.com/CPF18A/18pfa_lpthw-def.git\n"
+            "\n"
+            "사아자(2017958076) (03.11 오후 02:33)\n"
+            "\n"
+            "2017958076 사아자 라마바 https://github.com/CPF18A/18pfa_lpthw-ghi.git\n"
+        )
+
+        self.expected_list = [
+            '18pfa_lpthw-abc',
+            '18pfa_lpthw-def',
+            '18pfa_lpthw-ghi',
+        ]
+
+        self.list_file.seek(0)
+
+        self.config = configparser.ConfigParser()
+        self.config['operation'] = {
+            'sections':['a']
+        }
+
+        self.rel_path = 'rel_path'
+
+        self.config['a'] = {
+            'before': 'due date',
+            'folder': self.rel_path,
+            'list': self.list_file.name,
+        }
+
+    def tearDown(self):
+        del self.list_file
+        del self.config
+
+    def test_gen_arg_parser(self):
+        expected_full_path_list = [
+            os.path.join(os.getcwd(), self.rel_path, proj)
+            for proj in self.expected_list
+        ]
+
+        for path, due in cdd.gen_repo_path(self.config):
+            self.assertIn(path, expected_full_path_list)
+            self.assertEqual(due, self.config['a']['before'], msg=due)
 
 
 if "__main__" == __name__:
