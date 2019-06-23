db6df5dfe31790b4af54faefdab525b4f8000db5
KangWon LEE u64 <kangwon.lee.edu@gmail.com>
2019-06-24 00:33:59 +0900

    added iter_repo_path()

--- iter_repo.py
+++ iter_repo.py
@@ -2,6 +2,7 @@ import ast
 import os
 import re
 
+import repo_path
 
 def get_section_list(config):
     # https://stackoverflow.com/questions/335695/lists-in-configparser
@@ -42,3 +43,26 @@ def iter_github_urls(txt):
 def iter_github_urls_in_file(filename):
     for url in iter_github_urls(read_b_decode(filename)):
         yield url
+
+
+def iter_repo_path(config, b_assert=True):
+    """
+    Iterate over full paths to each local repository
+
+    """
+
+    for section in gen_section(config):
+        due_date = config[section]['before']
+        repo_path_rel = config[section]['folder']
+
+        for url in iter_github_urls_in_file(config[section]['list']):
+            proj_id = repo_path.get_repo_name_from_url(url)
+
+            full_path_to_repo = os.path.abspath(
+                os.path.join(repo_path_rel, proj_id)
+            )
+
+            if b_assert:
+                assert os.path.exists(full_path_to_repo), full_path_to_repo
+
+            yield full_path_to_repo, due_date
