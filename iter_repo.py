424dcf9924f4b0b30c93b7ac516bb0f07836d9c0
KangWon LEE u64 <kangwon.lee.edu@gmail.com>
2019-06-23 20:01:55 +0900

    added gen_section() to iter_repo

--- iter_repo.py
+++ iter_repo.py
@@ -4,3 +4,13 @@ import ast
 def get_section_list(config):
     # https://stackoverflow.com/questions/335695/lists-in-configparser
     return ast.literal_eval(config['operation']['sections'])
+
+
+def gen_section(config):
+    """
+    Iterate over sections of the config file:
+
+    """
+
+    for section in get_section_list(config):
+        yield section
