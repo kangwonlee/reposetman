3b5a652a1d06696228acc297308c4b2103c89af1
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-09-12 14:37:14 +0900

    content as first position arg

--- tempf.py
+++ tempf.py
@@ -11,7 +11,7 @@ def get_tempfile_name(suffix: str=None, istext: bool=True) -> str:
     return filename
 
 
-def write_to_temp_file(mode='wt', suffix='.py', encoding='utf-8', content=""):
+def write_to_temp_file(content: str, mode: str='wt', suffix: str='.py', encoding: str='utf-8',):
     with tempfile.NamedTemporaryFile(mode='wt', suffix='.py', encoding='utf-8', delete=False) as argv_file:
         argv_file.write(content)
     return argv_file
