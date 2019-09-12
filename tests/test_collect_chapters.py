60004a6ca72fa2750718d4f94c071c08ba524ba0
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-09-12 11:07:09 +0900

    rewrote tests in unittest
    \n at the end of each line to files
    
    remove files after test
    
    suffix = .txt

--- tests/test_collect_chapters.py
+++ tests/test_collect_chapters.py
@@ -2,8 +2,8 @@ import configparser
 import copy
 import os
 import pprint
-import pytest
 import sys
+import tempfile
 import unittest
 import urllib.parse as up
 
@@ -77,79 +77,134 @@ class TestCollectChapters(unittest.TestCase):
                 self.assertTrue(any(in_list))
 
 
-@pytest.fixture
-def sections_dict():
-    return {
-        'cls12a_00':{
-            'urls':[
-                'https://github.com/cls12a/cls12a-nmisp-00-abc',
-                'https://github.com/cls12a/cls12a-nmisp-00-def',
-                'https://github.com/cls12a/cls12a-nmisp-00-ghi-eca00a',
-            ]
-        },
-        'cls12a_01':{
-            'urls':[
-                'https://github.com/cls12a/cls12a-nmisp-01-abc',
-                'https://github.com/cls12a/cls12a-nmisp-01-def',
-                'https://github.com/cls12a/cls12a-nmisp-01-ghi-eca00a',
-            ]
-        },
-        'cls12a_tutorial':{
-            'urls':[
-                'https://github.com/cls12a/cls12a-nmisp-tutorial-abc',
-                'https://github.com/cls12a/cls12a-nmisp-tutorial-def',
-                'https://github.com/cls12a/cls12a-nmisp-tutorial-ghi-eca00a',
-            ]
-        },
-    }
+class TestSetUserIdsBase(unittest.TestCase):
+    def setUp(self):
+        self.sections_dict = self.get_sections_dict()
+        self.config = self.get_config()
 
-
-@pytest.fixture
-def config():
-    return configparser.ConfigParser(
-        {
-            'operation':{
-                'sections': "['cls12a_00', 'cls12a_01', 'cls12a_tutorial']"
+    @staticmethod
+    def get_sections_dict():
+        return {
+            'cls12a_00':{
+                'urls':[
+                    'https://github.com/cls12a/cls12a-nmisp-00-abc',
+                    'https://github.com/cls12a/cls12a-nmisp-00-def',
+                    'https://github.com/cls12a/cls12a-nmisp-00-ghi-eca00a',
+                ]
             },
-            'cls12a_00':{'repo_prefix': 'cls12a-nmisp-00-'},
-            'cls12a_01':{'repo_prefix': 'cls12a-nmisp-01-'},
-            'cls12a_tutorial':{'repo_prefix': 'cls12a-nmisp-tutorial-'},
+            'cls12a_01':{
+                'urls':[
+                    'https://github.com/cls12a/cls12a-nmisp-01-abc',
+                    'https://github.com/cls12a/cls12a-nmisp-01-def',
+                    'https://github.com/cls12a/cls12a-nmisp-01-ghi-eca00a',
+                ]
+            },
+            'cls12a_tutorial':{
+                'urls':[
+                    'https://github.com/cls12a/cls12a-nmisp-tutorial-abc',
+                    'https://github.com/cls12a/cls12a-nmisp-tutorial-def',
+                    'https://github.com/cls12a/cls12a-nmisp-tutorial-ghi-eca00a',
+                ]
+            },
         }
-    )
+
+    @staticmethod
+    def get_config():
+        config = configparser.ConfigParser()
+
+        config.read_dict(
+            {
+                'operation':{
+                    'sections': "['cls12a_00', 'cls12a_01', 'cls12a_tutorial']"
+                },
+                'cls12a_00':{'repo_prefix': 'cls12a-nmisp-00-'},
+                'cls12a_01':{'repo_prefix': 'cls12a-nmisp-01-'},
+                'cls12a_tutorial':{'repo_prefix': 'cls12a-nmisp-tutorial-'},
+            }
+        )
+
+        return config
 
 
-def test_set_user_ids(config, sections_dict):
-    url_parse_dict = {
-        'cls12a_00': [
-            up.urlparse('https://github.com/cls12a/cls12a-nmisp-00-abc'),
-            up.urlparse('https://github.com/cls12a/cls12a-nmisp-00-def'),
-            up.urlparse('https://github.com/cls12a/cls12a-nmisp-00-ghi-eca00a'),
-        ],
-        'cls12a_01': [
-            up.urlparse('https://github.com/cls12a/cls12a-nmisp-01-abc'),
-            up.urlparse('https://github.com/cls12a/cls12a-nmisp-01-def'),
-            up.urlparse('https://github.com/cls12a/cls12a-nmisp-01-ghi-eca00a'),
-        ],
-        'cls12a_tutorial': [
-            up.urlparse('https://github.com/cls12a/cls12a-nmisp-tutorial-abc'),
-            up.urlparse('https://github.com/cls12a/cls12a-nmisp-tutorial-def'),
-            up.urlparse('https://github.com/cls12a/cls12a-nmisp-tutorial-ghi-eca00a'),
-        ],
-    }
+class TestSetUserIds(TestSetUserIdsBase):
+    def test_set_user_ids(self):
 
-    storage = copy.deepcopy(sections_dict)
+        url_parse_dict = {
+            'cls12a_00': [
+                up.urlparse('https://github.com/cls12a/cls12a-nmisp-00-abc'),
+                up.urlparse('https://github.com/cls12a/cls12a-nmisp-00-def'),
+                up.urlparse('https://github.com/cls12a/cls12a-nmisp-00-ghi-eca00a'),
+            ],
+            'cls12a_01': [
+                up.urlparse('https://github.com/cls12a/cls12a-nmisp-01-abc'),
+                up.urlparse('https://github.com/cls12a/cls12a-nmisp-01-def'),
+                up.urlparse('https://github.com/cls12a/cls12a-nmisp-01-ghi-eca00a'),
+            ],
+            'cls12a_tutorial': [
+                up.urlparse('https://github.com/cls12a/cls12a-nmisp-tutorial-abc'),
+                up.urlparse('https://github.com/cls12a/cls12a-nmisp-tutorial-def'),
+                up.urlparse('https://github.com/cls12a/cls12a-nmisp-tutorial-ghi-eca00a'),
+            ],
+        }
 
-    col.set_user_ids(config, storage, url_parse_dict)
+        storage = copy.deepcopy(self.sections_dict)
 
-    expected = ['abc', 'def', 'ghi-eca00a']
+        col.set_user_ids(self.config, storage, url_parse_dict)
 
-    for _, value in storage.items:
-        assert 'user_ids' in value
-        assert value['user_ids'] == expected
+        expected_set = set(['abc', 'def', 'ghi-eca00a'])
+
+        for _, value in storage.items():
+            self.assertIn('user_ids', value)
+            self.assertSetEqual(set(value['user_ids']), expected_set)
 
 
-def test_get_sections_dict(config):
-    result = col.get_sections_dict(config)
+class TestGetSectionsDict(TestSetUserIdsBase):
+    def get_config(self):
+        config = super().get_config()
+
+        with tempfile.NamedTemporaryFile('wt', suffix='.txt') as lst_00:
+            lst_00_name = os.path.basename(lst_00.name)
+        with open(lst_00_name, 'wt', encoding='utf-8') as lst_00:
+            lst_00.write(
+                '123 abcde https://github.com/cls12a/cls12a-nmisp-00-abc\n'
+                '456 fghij https://github.com/cls12a/cls12a-nmisp-00-def\n'
+                '789 klmno https://github.com/cls12a/cls12a-nmisp-00-ghi-eca00a\n'
+            )
+
+        with tempfile.NamedTemporaryFile('wt', suffix='.txt') as lst_01:
+            lst_01_name = os.path.basename(lst_01.name)
+        with open(lst_01_name, 'wt', encoding='utf-8') as lst_01:
+            lst_01.write(
+                '456 fghij https://github.com/cls12a/cls12a-nmisp-10-def\n'
+                '123 abcde https://github.com/cls12a/cls12a-nmisp-10-abc\n'
+                '789 klmno https://github.com/cls12a/cls12a-nmisp-10-ghi-eca00a\n'
+            )
+
+        with tempfile.NamedTemporaryFile('wt', suffix='.txt') as lst_tutorial:
+            lst_tutorial_name = os.path.basename(lst_tutorial.name)
+        with open(lst_tutorial_name, 'wt', encoding='utf-8') as lst_tutorial:
+            lst_tutorial.write(
+                '789 klmno https://github.com/cls12a/cls12a-nmisp-tutorial-ghi-eca00a\n'
+                '456 fghij https://github.com/cls12a/cls12a-nmisp-tutorial-def\n'
+                '123 abcde https://github.com/cls12a/cls12a-nmisp-tutorial-abc\n'
+            )
+
+        config['cls12a_00']['list'] = lst_00_name
+        config['cls12a_01']['list'] = lst_01_name
+        config['cls12a_tutorial']['list'] = lst_tutorial_name
+
+        self.del_these_later = [lst_00_name, lst_01_name, lst_tutorial_name,]
+
+        return config
+
+    def tearDown(self):
+        [os.remove(name) for name in self.del_these_later]
+
+    def test_get_sections_dict(self):
+
+        result = col.get_sections_dict(self.config)
+
+        self.assertIsInstance(result, dict)
 
 
 if "__main__" == __name__:
