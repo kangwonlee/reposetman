2d2b3711159637f9a4b72e93fb35ba91ca540d86
KangWon LEE <kangwon.lee@kpu.ac.kr>
2019-10-13 18:19:55 +0900

    added TestArgParser

--- tests/test_argparse.py
+++ tests/test_argparse.py
@@ -13,3 +13,27 @@ sys.path.insert(
 )
 
 import progress
+
+
+class TestArgParser(unittest.TestCase):
+    def test_parse_args_usual(self):
+        config_filename = 'config.cfg'
+
+        input_arguments  = ['script.py', config_filename]
+        result_namespace = progress.parse_args(input_arguments)
+
+        self.assertEqual(config_filename, result_namespace.config_filename)
+        self.assertTrue(result_namespace.multiprocessing)
+
+    def test_parse_args_disable_multiprocessing(self):
+        config_filename = 'config.cfg'
+        input_arguments  = ['script.py', config_filename, '--no-multiprocessing',]
+        result_namespace = progress.parse_args(input_arguments)
+
+        self.assertTrue(result_namespace.config_filename)
+        self.assertTrue(result_namespace.config_filename.endswith('cfg'))
+        self.assertFalse(result_namespace.multiprocessing)
+
+
+if "__main__" == __name__:
+    unittest.main()
